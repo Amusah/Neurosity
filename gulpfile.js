ad1ac33b63a3952de7cf2d88f65439d614a1c609
Amusah <henryamusah@gmail.com>
2023-03-29 11:42:54 +0000

    Neurosity Homepage clone

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+compileCss = ()=>{
+    console.log('Compiling css');
+    return src('pages/**/*.scss') // scss source file
+    .pipe(sass()) //scss compiler
+    .pipe(dest('css')) // computed css
+}
+
+watchTask = () => {
+    watch(['pages/**/*.scss', '*.html'], compileCss);
+}
+
+exports.default = series(compileCss, watchTask);
+
+
+/*****
+NB: the * symbol means all files with the .scss and .html extention
+the double ** means all subfolders containing .scss files as well
+
+*****/
